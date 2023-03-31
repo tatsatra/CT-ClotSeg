1c0846d793bac29939f595970a3302be9b1965f5
tatsatra <tatsatra@buffalo.edu>
2023-03-31 15:24:59 -0400

    updated directories
    Added the data and time signatures to the directory names of all new model trainings to generate new and unique folders for saving model weights, history and hyper-parameters in unique directories.

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ Created on Fri Feb 17 14:52:41 2023
 """
 
 import os
-
+from datetime import datetime
 
 #########################################
 ########  FOR SLICING OF IMAGES  ########
 #########################################
 
-DIR_PATH = "D:/Munjal/Clot_DL/"
+DIR_PATH = os.getcwd()
 
-TRAIN_TEST_SPLIT = True
+TRAIN_TEST_SPLIT = False
 RANDOM_SEED = 1
 TRAIN_PERC = 0.6
 VAL_PERC = 0.2
@@ -24,12 +24,12 @@ MASK_DIR_IN = f"{DIR_PATH}/DL_seg_files/binary/"
 
 DIR_OUT = f"{DIR_PATH}/Processed/"
 
-SLICE_EXTRACT = True
+SLICE_EXTRACT = False
 
-TARGET_SAMPLE_SIZE_AUG_TRAIN = 100
-TARGET_SAMPLE_SIZE_AUG_VAL = 20
+TARGET_SAMPLE_SIZE_AUG_TRAIN = 10000
+TARGET_SAMPLE_SIZE_AUG_VAL = 2000
 
-DATA_AUG = True
+DATA_AUG = False
 NO_OF_TRANSFORMS = 8
 
 
@@ -37,7 +37,11 @@ MODEL_TRAINING = True
 INPUT_SHAPE = (256, 256, 2)
 OUTPUT_SHAPE = (256, 256, 1)
 BATCH_SIZE = 20
-EPOCHS = 5
-LEARNING_RATE = 0.0005
+EPOCHS = 100
+LEARNING_RATE = 0.001
 
-MODEL_OUT = f"{DIR_PATH}/TrainedModels/"
+dAndT = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+MODEL_OUT = f"{DIR_PATH}/Trainings/" + dAndT + "/Best_Model/"
+CKPT_OUT = f"{DIR_PATH}/Trainings/" + dAndT + "/Checkpoint_Files/"
+FILE_CP_OUT = f"{DIR_PATH}/Trainings/" + dAndT + "/Relevant_Files/"
